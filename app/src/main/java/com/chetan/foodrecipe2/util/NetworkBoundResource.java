539c77452b85f25fada502ffd90bd212593c45d8
chetan882777 <chetan882777@gmail.com>
2019-04-22 01:49:52 -0700

    Network Bound resource 2

--- app/src/main/java/com/chetan/foodrecipe2/util/NetworkBoundResource.java
+++ app/src/main/java/com/chetan/foodrecipe2/util/NetworkBoundResource.java
@@ -1,19 +1,59 @@
 package com.chetan.foodrecipe2.util;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MediatorLiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Observer;
 import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
 
+import com.chetan.foodrecipe2.AppExecutors;
 import com.chetan.foodrecipe2.requests.responses.ApiResponse;
+
+import retrofit2.Response;
 
 // CacheObject: Type for the Resource data. (database cache)
 // RequestObject: Type for the API response. ( network request)
 public abstract class NetworkBoundResource<CacheObject, RequestObject> {
 
-    private MutableLiveData<Resource<CacheObject>> results = new MutableLiveData<>();
+    private MediatorLiveData<Resource<CacheObject>> results = new MediatorLiveData<>();
+    private AppExecutors appExecutors;
+
+    public NetworkBoundResource(AppExecutors appExecutors){
+        this.appExecutors = appExecutors;
+        init();
+    }
+
+    private void init(){
+        results.setValue((Resource<CacheObject>) Resource.loading(null));
+
+        final LiveData<CacheObject> dbSource = loadFromDb();
+
+        results.addSource(dbSource, new Observer<CacheObject>() {
+            @Override
+            public void onChanged(@Nullable CacheObject cacheObject) {
+                results.removeSource(dbSource);
+                if(shouldFetch(cacheObject)){
+
+                }else{
+                    results.addSource(dbSource, new Observer<CacheObject>() {
+                        @Override
+                        public void onChanged(@Nullable CacheObject cacheObject) {
+                            setValue(Resource.success(cacheObject));
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void setValue(Resource<CacheObject> newValue) {
+        if(results.getValue() != newValue){
+            results.setValue(newValue);
+        }
+    }
 
 
     // Called to save the result of the API response into the database.
