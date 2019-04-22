c9ce143a3b088982b2da1b475f2bd00ebc9ae0e7
chetan882777 <chetan882777@gmail.com>
2019-04-22 08:58:55 -0700

    Room type converters

--- app/src/main/java/com/chetan/foodrecipe2/persistence/RecipeDatabase.java
+++ app/src/main/java/com/chetan/foodrecipe2/persistence/RecipeDatabase.java
@@ -3,11 +3,13 @@ package com.chetan.foodrecipe2.persistence;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
 import android.content.Context;
 
 import com.chetan.foodrecipe2.models.Recipe;
 
 @Database(entities = {Recipe.class}, version = 1)
+@TypeConverters({Converters.class})
 public abstract class RecipeDatabase extends RoomDatabase {
 
     public static final String DATABASE_NAME = "reicpes_db";
