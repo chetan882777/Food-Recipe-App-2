6885c9cd93cfacf538cb4a8a794a6dbd6e842d33
chetan882777 <chetan882777@gmail.com>
2019-04-22 08:09:08 -0700

    Room Database

---
+++ app/src/main/java/com/chetan/foodrecipe2/persistence/RecipeDatabase.java
@@ -0,0 +1,28 @@
+package com.chetan.foodrecipe2.persistence;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.chetan.foodrecipe2.models.Recipe;
+
+@Database(entities = {Recipe.class}, version = 1)
+public abstract class RecipeDatabase extends RoomDatabase {
+
+    public static final String DATABASE_NAME = "reicpes_db";
+
+    private static RecipeDatabase instance;
+
+    public RecipeDatabase getInstance(final Context context){
+        if(instance == null){
+            instance = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    RecipeDatabase.class,
+                    DATABASE_NAME)
+                    .build();
+        }
+        return instance;
+    }
+
+}
