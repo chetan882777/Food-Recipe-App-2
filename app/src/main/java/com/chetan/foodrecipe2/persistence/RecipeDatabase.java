3f9eed14a72a24948689104aa3ceb56faec9102c
chetan882777 <chetan882777@gmail.com>
2019-04-22 10:34:37 -0700

    Repository setup

--- app/src/main/java/com/chetan/foodrecipe2/persistence/RecipeDatabase.java
+++ app/src/main/java/com/chetan/foodrecipe2/persistence/RecipeDatabase.java
@@ -12,11 +12,11 @@ import com.chetan.foodrecipe2.models.Recipe;
 @TypeConverters({Converters.class})
 public abstract class RecipeDatabase extends RoomDatabase {
 
-    public static final String DATABASE_NAME = "reicpes_db";
+    public static final String DATABASE_NAME = "recipes_db";
 
     private static RecipeDatabase instance;
 
-    public RecipeDatabase getInstance(final Context context){
+    public static RecipeDatabase getInstance(final Context context){
         if(instance == null){
             instance = Room.databaseBuilder(
                     context.getApplicationContext(),
---
+++ app/src/main/java/com/chetan/foodrecipe2/repositories/RecipeRepository.java
@@ -0,0 +1,69 @@
+package com.chetan.foodrecipe2.repositories;
+
+import android.arch.lifecycle.LiveData;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.chetan.foodrecipe2.AppExecutors;
+import com.chetan.foodrecipe2.models.Recipe;
+import com.chetan.foodrecipe2.persistence.RecipeDao;
+import com.chetan.foodrecipe2.persistence.RecipeDatabase;
+import com.chetan.foodrecipe2.requests.responses.ApiResponse;
+import com.chetan.foodrecipe2.requests.responses.RecipeSearchResponse;
+import com.chetan.foodrecipe2.util.NetworkBoundResource;
+import com.chetan.foodrecipe2.util.Resource;
+
+import java.util.List;
+
+public class RecipeRepository {
+
+    private static RecipeRepository instance;
+    private RecipeDao recipeDao;
+
+    public static RecipeRepository getInstance(Context context){
+        if(instance == null){
+            instance = new RecipeRepository(context);
+        }
+        return instance;
+    }
+
+
+    private RecipeRepository(Context context) {
+        recipeDao = RecipeDatabase.getInstance(context).getRecipeDao();
+    }
+
+    public LiveData<Resource<List<Recipe>>> searchRecipesApi(final String query, final int pageNumber){
+        return new NetworkBoundResource<List<Recipe>, RecipeSearchResponse>(AppExecutors.getInstance() ){
+
+            @Override
+            public void saveCallResult(@NonNull RecipeSearchResponse item) {
+
+            }
+
+            @Override
+            public boolean shouldFetch(@Nullable List<Recipe> data) {
+                return true; // always query the network since the queries can be anything
+            }
+
+            @NonNull
+            @Override
+            public LiveData<List<Recipe>> loadFromDb() {
+                return recipeDao.searchRecipes(query, pageNumber);
+            }
+
+            @NonNull
+            @Override
+            public LiveData<ApiResponse<RecipeSearchResponse>> createCall() {
+                return null;
+            }
+
+        }.getAsLiveData();
+    }
+}
+
+
+
+
+
+
