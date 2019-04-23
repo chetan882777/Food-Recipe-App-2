60b23f0a3f366da74d9669f1ab01bd6ea5681d81
chetan882777 <chetan882777@gmail.com>
2019-04-23 12:31:00 -0700

    OkHttp Network Requests

--- app/src/main/java/com/chetan/foodrecipe2/requests/ServiceGenerator.java
+++ app/src/main/java/com/chetan/foodrecipe2/requests/ServiceGenerator.java
@@ -3,14 +3,29 @@ package com.chetan.foodrecipe2.requests;
 import com.chetan.foodrecipe2.util.Constants;
 import com.chetan.foodrecipe2.util.LiveDataCallAdapterFactory;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static com.chetan.foodrecipe2.util.Constants.CONNECTION_TIMEOUT;
+import static com.chetan.foodrecipe2.util.Constants.READ_TIMEOUT;
+import static com.chetan.foodrecipe2.util.Constants.WRITE_TIMEOUT;
+
 public class ServiceGenerator {
+
+    private static OkHttpClient client = new OkHttpClient.Builder()
+            .connectTimeout(CONNECTION_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+            .build();
+
 
     private static Retrofit.Builder retrofitBuilder =
             new Retrofit.Builder()
                     .baseUrl(Constants.BASE_URL)
+                    .client(client)
                     .addCallAdapterFactory(new LiveDataCallAdapterFactory())
                     .addConverterFactory(GsonConverterFactory.create());
 
--- app/src/main/java/com/chetan/foodrecipe2/util/Constants.java
+++ app/src/main/java/com/chetan/foodrecipe2/util/Constants.java
@@ -7,6 +7,9 @@ public class Constants {
     // YOU NEED YOUR OWN API KEY!!!!!!!!!!!!! https://www.food2fork.com/about/api
     public static final String API_KEY = "7e492070bb89e08a95b8a5b4c38e8eb7";
 
+    public static final int CONNECTION_TIMEOUT = 10; // 10seconds
+    public static final int READ_TIMEOUT = 10; // 10seconds
+    public static final int WRITE_TIMEOUT = 10; // 10seconds
 
     public static final String[] DEFAULT_SEARCH_CATEGORIES =
             {"Barbeque", "Breakfast", "Chicken", "Beef", "Brunch", "Dinner", "Wine", "Italian"};
